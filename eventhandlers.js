641ee3fea82a0cce1ddf30bd612c8c575a93b66a
Alan Guo <azguo@andrew.cmu.edu>
2017-12-23 15:30:37 -0500

    javascript file

---
+++ eventhandlers.js
@@ -0,0 +1,5 @@
+var pbutton = document.getElementById("projects-button");
+var pmodal = document.getElementById("projects-modal");
+pbutton.onclick = function() {
+    pmodal.style.display="flex";
+}
